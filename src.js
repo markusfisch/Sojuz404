8c157ca1d18dc66666f7b4a7bd38543d9b80ba5e
Markus Fisch <mf@markusfisch.de>
2020-09-01 22:47:11 +0200

    Test the concept

--- src.js
+++ src.js
@@ -1,8 +1,92 @@
 'use strict'
 
-const D = document,
-	W = window
+const W = window,
+	M = Math
+
+let animationRequestId,
+	stageWidth,
+	stageHeight,
+	stage,
+	scene,
+	soyus
+
+function draw() {
+	const cx = stageWidth * .5,
+		cy = stageHeight * .5,
+		t = Date.now() * .001,
+		x = cx - 50,
+		y = cy + M.sin(t) * 4 - 50
+	soyus.style.transformOrigin = '50px 50px'
+	soyus.style.transform = `translate(${x}px, ${y}px) rotateZ(22deg)`
+}
+
+function run() {
+	animationRequestId = requestAnimationFrame(run)
+	draw()
+}
+
+function resize() {
+	if (animationRequestId) {
+		cancelAnimationFrame(animationRequestId)
+	}
+	const windowWidth = window.innerWidth,
+		windowHeight = window.innerHeight,
+		min = M.min(windowWidth, windowHeight),
+		scale = min / 300,
+		style = stage.style
+	stageWidth = windowWidth / scale
+	stageHeight = windowHeight / scale
+	style.width = stageWidth + 'px'
+	style.height = stageHeight + 'px'
+	style.transformOrigin = 'top left'
+	style.transform = `scale(${scale})`
+	style.display = 'block'
+	run()
+}
+
+function pointerInspect(event) {
+	event.stopPropagation()
+}
+
+function pointerInteract(event) {
+	event.stopPropagation()
+}
+
+function pointerCancel(event) {
+	event.stopPropagation()
+}
 
 W.onload = function() {
-	D.getElementById('Canvas')
+	const D = document
+
+	stage = D.getElementById('Stage')
+	scene = D.getElementById('Scene')
+	soyus = D.getElementById('Soyus')
+
+	W.onresize = resize
+	resize()
+
+	D.onmousedown = pointerInspect
+	D.onmousemove = pointerInspect
+	D.onmouseup = pointerInteract
+	D.onmouseout = pointerCancel
+
+	if ('ontouchstart' in D) {
+		D.ontouchstart = pointerInspect
+		D.ontouchmove = pointerInspect
+		D.ontouchend = pointerInteract
+		D.ontouchleave = pointerCancel
+		D.ontouchcancel = pointerCancel
+
+		// prevent pinch/zoom on iOS 11
+		D.addEventListener('gesturestart', function(event) {
+			event.preventDefault()
+		}, false)
+		D.addEventListener('gesturechange', function(event) {
+			event.preventDefault()
+		}, false)
+		D.addEventListener('gestureend', function(event) {
+			event.preventDefault()
+		}, false)
+	}
 }
